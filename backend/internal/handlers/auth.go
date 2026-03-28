6ba8f93aa4c5d8b97e8828106f85012c8111fffc
Artem <arvlas78@gmail.com>
2026-03-28 14:06:38 +0200

    chore: removed unrelevant comment

--- backend/internal/handlers/auth.go
+++ backend/internal/handlers/auth.go
@@ -106,7 +106,6 @@ func (h *AuthHandler) Logout(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "logout successful"})
 }
 
-// TODO: those hardcoded field names... use constants
 func (h *AuthHandler) Me(c *gin.Context) {
 	userID, _ := c.Get(models.UserIDKey)
 	userRole, _ := c.Get(models.UserRoleKey)
